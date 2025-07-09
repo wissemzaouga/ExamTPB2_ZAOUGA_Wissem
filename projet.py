14fc913e3808f7fc1bbd2fc47ea3919139366153
wissem zaouga <wissem.zaouga@edu.isetcom.com>
2025-07-09 19:54:00 +0100

    commit question 13

--- projet.py
+++ projet.py
@@ -4,6 +4,11 @@ for t in T:
 S*=t
 return S
 Data=[1,3,5]
+
 Prod=math.prod (Data)
 print ('le produit est:',Prod)
+
+print ('le produit est:',Prod(DATA))
+print ('le min est:', min(DATA))
+print ('le max est:',max(DATA))
 
